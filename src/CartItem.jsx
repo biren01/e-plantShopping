fea358c4be27272471b185fbaf0f3168b158d0e2
biren <raibiren@gmail.com>
2026-01-28 16:58:57 -0500

    task4-2, task 4-3 completed

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -17,10 +17,14 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleContinueShopping = (e) => {
+    e.preventDefault();
+    onContinueShopping(e);
    
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
   };
